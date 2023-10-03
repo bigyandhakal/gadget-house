1a53aa3e9d7393bda9136e9906c229b7fb9bc830
Bigyan Dhakal <dhakalbigyan0@gmail.com>
2023-10-03 09:27:25 +0545

    routing setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,29 @@
+import { createBrowserRouter, BrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Error from "./pages/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Home/>
+  },
+  {
+    path:"/about",
+    element:<About/>
+  },
+  {
+    path:"*",
+    element:<Error/>
+  }
+]);
+
 function App() {
   return (
     <>
-      Hello World
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
