18fedd9bc25b20df8c788b9e0782b5e209de4c3c
Bigyan Dhakal <dhakalbigyan0@gmail.com>
2023-11-21 22:47:39 +0545

    changes

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -10,6 +10,7 @@ function Paginate() {
       <Pagination.Item>{3}</Pagination.Item>
       <Pagination.Item>{4}</Pagination.Item>
       <Pagination.Item>{5}</Pagination.Item>
+      <Pagination.Item>{6}</Pagination.Item>
       <Pagination.Next />
       <Pagination.Last />
     </Pagination>
